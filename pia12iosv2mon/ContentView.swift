a89c921bf68144a93e36ea802aa5ab37d9c131ec
MiamiPublication <145936310+MiamiPublication@users.noreply.github.com>
2023-11-08 15:21:54 +0100

    Update ContentView.swift

--- pia12iosv2mon/ContentView.swift
+++ pia12iosv2mon/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hej våld")
+            Text("Hej våld dfdf")
             Text("Mere text som ändrats")
         }
         .padding()
