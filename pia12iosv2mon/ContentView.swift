cc877a4cfb237283f9b7fef5198b7b47ea0b64be
Miami Jaboori <b.jaboori@gmail.com>
2023-11-08 15:23:00 +0100

    uttropstakeb

--- pia12iosv2mon/ContentView.swift
+++ pia12iosv2mon/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hej våld")
-            Text("Mere text som ändrats")
+            Text("Mere text som ändrats!!!!")
         }
         .padding()
     }
